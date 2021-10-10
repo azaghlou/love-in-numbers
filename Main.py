724aa659ed29cb0f246efdecc7120bd5812e29f9
beel <abdallah98@gmail.com>
2021-10-10 18:51:21 +0200

    yella

--- Main.py
+++ Main.py
@@ -1,1 +1,19 @@
-print("hello")
+import random
+
+import simpy
+import statistics
+
+
+class Partners(object):
+    def __init__(self):
+        self.ayzeh = 10
+        self.badyeh = 10
+        self.worth = random.random()
+        self.mawada = 1
+        self.value = (self.mawada / self.worth) * self.ayzeh
+
+    def BreakUp(self):
+        if self.value >= self.ayzeh:
+            return False
+        else:
+            return True
